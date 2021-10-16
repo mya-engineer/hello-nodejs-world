08c7e594af866b1b16b7e70fd2f3948b519353d6
mya <yatsenkomy@yandex.ru>
2021-10-16 16:13:54 +0300

    ejs-viewEngine added

--- application/app.js
+++ application/app.js
@@ -3,24 +3,49 @@ const path = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 const PORT = 8888
 
-const createPath = page => path.resolve(__dirname, 'views', `${page}.html`)
+const createPath = page => path.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'))
+  const title = 'Home'
+  res.render(createPath('index'), { title })
 })
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'))
+  const title = 'Contacts'
+  const contacts = [
+    { name: 'ME', link: 'https://github.com/mya-engineer/hello-nodejs-world' },
+    { name: 'ME', link: 'https://github.com/mya-engineer/hello-nodejs-world' },
+    { name: 'ME', link: 'https://github.com/mya-engineer/hello-nodejs-world' },
+  ]
+  res.render(createPath('contacts'), { contacts, title })
 })
 
 app.get('/about', (req, res) => {
   res.redirect('/contacts')
 })
 
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post'
+  res.render(createPath('post'), { title })
+})
+
+app.get('/posts', (req, res) => {
+  const title = 'Posts'
+  res.render(createPath('posts'), { title })
+})
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post'
+  res.render(createPath('add-post'), { title })
+})
+
 app.use((req, res) => {
-  res.sendFile(createPath('error'))
+  const title = 'Error!'
+  res.render(createPath('error'), { title })
 })
 
 app.listen(PORT, error => {
