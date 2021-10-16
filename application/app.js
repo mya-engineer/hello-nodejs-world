45b0c4e70e2b3475fa25b7bb0b9069404227af5e
mya <yatsenkomy@yandex.ru>
2021-10-16 15:55:59 +0300

    simple express added

--- application/app.js
+++ application/app.js
@@ -1,49 +1,28 @@
-const http = require('http')
-const fs = require('fs')
+const express = require('express')
 const path = require('path')
+
+const app = express()
 
 const PORT = 8888
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method)
+const createPath = page => path.resolve(__dirname, 'views', `${page}.html`)
 
-  let basePath = ''
-
-  res.setHeader('Content-Type', 'text/html')
-
-  const createPath = page => path.resolve(__dirname, 'views', `${page}.html`)
-
-  res.statusCode = 200
-
-  switch (req.url) {
-    case '/':
-      basePath = createPath('index')
-      break
-    case '/about':
-      res.statusCode = 301
-      res.setHeader('Location', '/contacts')
-      res.end()
-      break
-    case '/contacts':
-      basePath = createPath('contacts')
-      break
-    default:
-      basePath = createPath('error')
-      res.statusCode = 404
-      break
-  }
-
-  fs.readFile(basePath, (err, data) => {
-    if (err) {
-      console.error(err)
-      res.statusCode = 500
-    } else {
-      res.write(data)
-    }
-    res.end()
-  })
+app.get('/', (req, res) => {
+  res.sendFile(createPath('index'))
 })
 
-server.listen(PORT, error => {
-  error ? console.error(error) : console.log(`Listening on port ${PORT}...`)
+app.get('/contacts', (req, res) => {
+  res.sendFile(createPath('contacts'))
+})
+
+app.get('/about', (req, res) => {
+  res.redirect('/contacts')
+})
+
+app.use((req, res) => {
+  res.sendFile(createPath('error'))
+})
+
+app.listen(PORT, error => {
+  error ? console.error(error) : console.log(`Listening on ${PORT}`)
 })
